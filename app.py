0428f3abdda207567a73d53511559cc9d681138b
Julien Chaumond <julien@huggingface.co>
2022-06-07 13:21:28 +0200

    First version

---
+++ app.py
@@ -0,0 +1,14 @@
+import gradio as gr
+
+
+def greet(name):
+    return "Hello " + name + "!"
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=gr.Textbox(lines=2, placeholder="Name Here..."),
+    outputs="text",
+)
+
+demo.launch(server_port=8080)
