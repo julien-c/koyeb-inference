ec9a3b215f4bfd572138408093c010964b3873c5
Julien Chaumond <julien@huggingface.co>
2022-06-07 13:45:15 +0200

    Perform inference

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 import gradio as gr
+from transformers import pipeline
+
+pipe = pipeline("sentiment-analysis")
 
 
-def greet(name):
-    return "Hello " + name + "!"
+def greet(text: str):
+    return pipe(text)
 
 
 demo = gr.Interface(
     fn=greet,
     inputs=gr.Textbox(lines=2, placeholder="Name Here..."),
-    outputs="text",
+    outputs="json",
+    examples=[
+        "I love this",
+        "I hate this",
+    ],
 )
 
 demo.launch(server_port=8080)
